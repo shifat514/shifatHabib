dd3c984b912bd21c06ed57b2929d2a59464bc4f8
shifat514 <shifat514@gmail.com>
2024-07-13 23:31:51 +0600

    fix: fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: cloudflare(),
-  integrations: [tailwind()]
+  integrations: [ tailwind()]
 });
